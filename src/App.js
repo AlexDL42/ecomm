8f7737adaacfe0dcf496a3e1a80ce56ec91f08ec
AlexDL42 <adl42.pro@gmail.com>
2020-04-15 13:17:43 +0100

    update our menu-item to navigate withRouter HighOrderComponent (HOC)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const MenPage = () => (
+  <div>
+    <h1>Men PAGE</h1>
+  </div>
+);
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+          <Route path='/men' component={MenPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
